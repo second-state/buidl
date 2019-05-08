060edfea24637e692093fad1a1ce7c783304f88d
wangshishuo <wangshishuo@gmail.com>
2019-05-08 17:21:18 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Home></Home>
   </div>
 </template>
+
+<script>
+import normalize from "normalize-styl";
+import Home from "@/views/Home.vue";
+
+export default {
+  components: {
+    Home
+  }
+};
+</script>
+
 <style lang="stylus">
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
@@ -14,12 +22,4 @@
   -moz-osx-font-smoothing grayscale
   text-align center
   color #2c3e50
-
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
+  el: "#app",
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => {
+    return h(App);
+  }
+});
