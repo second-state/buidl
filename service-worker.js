51440e046bc2ffd0d897e11f89533fc815bee37f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-20 09:16:16 +0000

    Deploy second-state/buidl to github.com/second-state/buidl.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aaa0a69413f129b33b29f39684d19715.js"
+  "/precache-manifest.369736262aea79d4b2c4950ad94d885e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "buidl"});
