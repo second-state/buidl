c4ee5a6bba3ffce7c71d3e3259028e7c648ade49
wangshishuo <wangshishuo@gmail.com>
2019-05-20 21:13:44 +0800

    resize pane

--- src/components/ResizeBar.vue
+++ src/components/ResizeBar.vue
@@ -8,6 +8,7 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { debounce } from "lodash";
 
 @Component({
   props: {
@@ -17,6 +18,7 @@ import { Component, Vue } from "vue-property-decorator";
 export default class ResizeBar extends Vue {
   startPoint = 0;
   startSize = 0;
+  _debouncedResize: EventListenerOrEventListenerObject | undefined = undefined;
 
   mousedown(e: any) {
     const element = this.$parent.$el;
@@ -28,24 +30,30 @@ export default class ResizeBar extends Vue {
       this.startPoint = e.pageY;
       this.startSize = dim.height;
     }
-    document.body.addEventListener("mousemove", this.mousemove);
+    this._debouncedResize = debounce(this.resize, 0, {maxWait: 100});
+    document.body.addEventListener("mousemove", this._debouncedResize);
     document.body.addEventListener("mouseup", this.mouseup);
+    document.body.className = `${this.$props.resizeDirection}-resizing`;
   }
 
   mouseup() {
-    document.body.removeEventListener("mousemove", this.mousemove);
+    if (this._debouncedResize) {
+      document.body.removeEventListener("mousemove", this._debouncedResize);
+    }
     document.body.removeEventListener("mouseup", this.mouseup);
+    document.body.className = "";
   }
 
-  mousemove(e: any) {
+  resize(e: any) {
     const element = this.$parent.$el;
     if (this.$props.resizeDirection === "horizontal") {
       let width = this.startSize + e.pageX - this.startPoint;
-      element.setAttribute("style", `width:${width}px`);
+      (element as any).style.width = `${width}px`;
     } else {
       let height = this.startSize + this.startPoint - e.pageY;
-      element.setAttribute("style", `height:${height}px`);
+      (element as any).style.height = `${height}px`;
     }
+    this.$store.dispatch("events/triggerEditorResize");
   }
 }
 </script>
@@ -58,12 +66,18 @@ export default class ResizeBar extends Vue {
     right 0
     top 0
     bottom 0
-    width 2px
+    width 3px
     cursor col-resize
   &.vertical
     top 0
     left 0
     right 0
-    height 2px
+    height 3px
     cursor row-resize
+
+body
+  &.horizontal-resizing *
+    cursor col-resize !important
+  &.vertical-resizing *
+    cursor row-resize !important
 </style>
