10ecbae75536d29cb809638d18dea0490c2237b1
wangshishuo <wangshishuo@gmail.com>
2021-07-26 05:54:33 +0000

    Deploying to gh-pages from @ second-state/buidl@69023dda93afb876ad960a90d4ab0c8a391ea620 🚀

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 
 importScripts(
   "sw-supplement.js",
-  "/precache-manifest.af8c2384a23c4ef9afba3ae7c268f76f.js"
+  "/precache-manifest.a4072a9729e962e9cdcda156726c0e27.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "buidl"});
